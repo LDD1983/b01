fadd18143005fff5e01873050e650ed7c80c85b1
mackliu <macklun@gmail.com>
2023-06-12 13:18:56 +0800

    完成class 中的save()及del()

--- base.php
+++ base.php
@@ -53,15 +53,47 @@ class DB{
 
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
+    
     function save($arg){
+        if(isset($arg['id'])){
+            //update
+            foreach($arg as $key => $value){
+                $tmp[]="`$key`='$value'";
+            }
 
-    }
-    function del(...$arg){
+            $sql="update $this->table set ".join(',',$tmp)." where `id`='{$arg['id']}'";
+        }else{
+            //insert
 
+            $cols=array_keys($arg);
+
+            $sql="insert into  $this->table (`".join("`,`",$cols)."`) 
+                                      values('".join("','",$arg)."')";
+        }
+
+        return $this->pdo->exec($sql);
     }
+    function del($arg){
+        $sql="delete from $this->table ";
+
+            if(is_array($arg)){
+
+                foreach($arg as  $key => $value){
+                    $tmp[]="`$key`='$value'";
+                }
+
+                $sql=$sql . " where " . join(" && " ,$tmp);
+            }else{
+                $sql=$sql . $arg;
+            }
+
+        return $this->pdo->exec($sql);
+    }
+
     function count(...$arg){
 
     }
+
     function math($math,$cols,...$arg){
 
     }
